6008826017d1d791832e3e189b6c5043bf8fdbe7
zunopark <enzoribery@naver.com>
2023-06-03 17:22:31 +0900

    add: 임시 커밋

---
+++ src/component/Footer.js
@@ -0,0 +1,22 @@
+export const Footer = () => {
+    return `<footer class="bg-[#000]">
+    <div class="container mx-auto lg:px-[40px] px-[10px] py-[30px] ease-out duration-300">
+        <div class="flex flex-wrap">
+            <div class="lg:w-[60%] w-[100%] text-[#eee] lg:text-[15px] text-[13px] lg:leading-[30px] leading-[24px]">
+                <span class="inline-block pr-[20px] ease-out duration-300">㈜BELL GROUND GYM</span>
+                <span class="inline-block pr-[20px] ease-out duration-300">000-00-00000</span>
+                <span class="inline-block pr-[20px] ease-out duration-300">[13487] 경기도 성남시 분당구 판교로228번길 15 판교세븐벤처벨리 2층</span>
+                <span class="inline-block pr-[20px] ease-out duration-300">example@gmail.com</span>
+            </div>
+            <div class="flex lg:justify-end justify-start lg:w-[40%] w-[100%] lg:mt-[0] mt-[20px]">
+                <a href="#" class="inline-block bg-[url('../dist/img/icon_insta.png')] bg-no-repeat bg-center bg-cover lg:w-[46px] w-[38px] lg:h-[46px] h-[38px] text-[0] ease-out duration-300">인스타그램</a>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#111]">
+        <div class="container mx-auto lg:px-[40px] px-[10px] py-[10px] ease-out duration-300 text-right">
+            <span class="text-[#989898] lg:text-[16px] text-[14px] font-[200]">Powered by <a href="#" class="lg:ml-[10px] ml-[6px] text-[#eee] lg:text-[20px] text-[18px] font-[800]  ease-out duration-300">ZNPYNK</a></span>
+        </div>
+    </div>
+</footer>`
+}
